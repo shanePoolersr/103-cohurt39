89324311500c24ce8aeb8d9dc61c79d887e8f21d
shanePoolersr <135184408+shanePoolersr@users.noreply.github.com>
2023-06-28 16:58:23 -0700

    This for assignment 1# 103

---
+++ script12.js
@@ -0,0 +1,39 @@
+console.log("Script");
+
+var user = "Shane";
+var age = 52;
+var isStudent = true;
+
+
+document.write(`
+<p>Name: ${user}</p>
+<p>Age: ${age}</p>
+<p>Active student: ${isStudent}</p>
+`);
+
+let jobTitle = "Graphic Designer";
+let geoLocation = "El Cajon";
+let partnerName = "Debbie";
+let numberChildren = "5";
+let numberOfGrandChildren = "5";
+let petNames = "Rambo, Deja, Loki, Jasmine"
+let myFavoriteHoliday = "Christmas"
+let myFavoriteFood = "Bar B Que"
+let myFavoriteColor = "blue"
+let myFavoriteSport = "football"
+let myFavoriteSportsTeam = "Chargers"
+let myFavoriteCollegeSportsTeam = "LSU Tigers"
+let myHobbies = "gym and  movies"
+
+document.write(`
+<p>I live in ${geoLocation} with my wife ${partnerName}.</p> 
+<p>We have ${numberChildren} children and ${numberOfGrandChildren} grand children not all of them live with us.</p>
+<p>We have 3 cats and one dog their names are ${petNames} most of the time they all get along.</p>
+<p>I work out of ${geoLocation} and I am a ${jobTitle} that wants to learn how to code.</p>
+<p>Some things about me my favorite food is ${myFavoriteFood} I can eat it seven days a week.</p>
+<p>My favorite color is ${myFavoriteColor}, my favorite sport is ${myFavoriteSport}, my favorite college football team is ${myFavoriteCollegeSportsTeam}.</p>
+<p>Although they left San Diego I am still ${myFavoriteSportsTeam} fan. When I am not busy I spent my time going to the ${myHobbies}.</p>
+<p>Last but not least my favorite holiday is ${myFavoriteHoliday} because you get to give people presents and see them smile.</p>
+
+
+`)
